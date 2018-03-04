422d0a7804a04228a216fe32be65b4332dcf3cc7
parhat93 <parhat93@gmail.com>
2018-03-04 17:00:22 -0500

    added if and else statement

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,6 +3,13 @@ package test;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("what ");
+		System.out.println("what");
+		int age = 5;
+		if(age==5) {
+			System.out.println("Wow you are very young boy");
+		}else if(age>5) {
+			System.out.println("you are not kid anymore");
+		}
+		
 	}
 }
