7593e67faf3d268d11772b4fe854705046bbe090
parhat93 <parhat93@gmail.com>
2018-03-10 12:48:18 -0500

    new print statement

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -2,6 +2,8 @@ package test;
 
 public class Hello {
 	public static void main(String[] args) {
+		
+		
 		System.out.println("Hello World");
 		System.out.println("what");
 		int age = 5;
@@ -9,6 +11,7 @@ public class Hello {
 			System.out.println("Wow you are very young boy");
 		}else if(age>5) {
 			System.out.println("you are not kid anymore");
+			System.out.println(age);
 		}
 		
 	}
